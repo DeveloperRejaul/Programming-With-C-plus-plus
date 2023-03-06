126b2cd5e47bd0090bf82f0ea989fb4f85df10b3
rejaul <devrejaul.official@gmail.com>
2023-03-06 08:42:28 +0600

    first commit

---
+++ 01.Hello-World-Programme/app.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
+
+
+
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/DeveloperRejaul/Programming-With-C-.git
+git push -u origin main
+
+
+
