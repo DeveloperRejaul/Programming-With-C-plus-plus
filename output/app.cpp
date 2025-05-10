02cbefe35373dbcaa4b589959325acc7ef7a2d1b
Rezaul Karim <rezaul@orangetoolz.com>
2025-05-10 14:25:30 +0600

    add

---
+++ output/app.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "Hello World";
+    cout << "I am learning C++";
+    
+    
+    // New line 
+    cout << "Hello World! \n";
+    cout << "I am learning C++ \n";
+
+
+    // print number 
+    cout << 3;
+
+    return 0;
+}
